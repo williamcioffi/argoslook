db22315cb5763788e8766a0e27a6241017d65a3c
williamcioffi <31380361+williamcioffi@users.noreply.github.com>
2017-08-27 14:15:40 +0000

    Add files via upload

---
+++ guts/argoslook.R
@@ -0,0 +1,274 @@
+#v 0.91
+require(leaflet)
+require(shiny)
+# require(ggmap)
+source("guts/datafunctions.R")
+#load("guts/mapzoomdat_small")
+gm_baseline <- read.table("guts/allgms.csv", header = TRUE, sep = ',')
+zc_baseline <- read.table("guts/allzcs.csv", header = TRUE, sep = ',')
+zc_baseline <- zc_baseline[-which(zc_baseline$dur > 10^5), ]
+base <- list(zc_baseline, gm_baseline)
+
+rm(list = c("gm_baseline", "zc_baseline"))
+
+ui <- fluidPage(
+	sidebarLayout(
+		sidebarPanel(
+			actionButton("quit", "quit"),
+			hr(),
+			# p("use forms below to upload behavior and argos files exported from DAP processor."),
+			# fileInput("sourcedir", "",
+				# accept = c("text/csv", "text/coma-separate-values", "text/plain",
+				# ".csv"), multiple = TRUE, width = "225"),
+			
+			fileInput("file1", "choose behavior csv:",
+				accept = c(
+				"text/csv",
+				"text/comma-separated-values,text/plain",
+				".csv"), width = "225"),
+			fileInput("file2", "choose argos csv:",
+				accept = c(
+				"text/csv",
+				"text/comma-separated-values,text/plain",
+				".csv"), width = "225"),
+				uiOutput("tagselect"),
+			selectInput("basesp", "baseline species", c("beakers", "pdubs", "other"), width = "150px"),
+			width = 3),
+
+		mainPanel(
+			tabsetPanel(
+				tabPanel("dive plot",
+					# actionButton("prev", "prev day"),
+					# actionButton("nxt", "next day"), 
+					hr(),
+					plotOutput("plot"),
+					plotOutput("legend")),
+				tabPanel("gap stats",
+					tableOutput("gapstatstable")),
+				tabPanel("dive stats",
+					uiOutput("baseline_mindep"),
+					plotOutput("divedepdur", height = "800px"),
+					tableOutput("divestatstable")),
+				# tabPanel("pos table",
+					# uiOutput("qflags2"),
+					# tableOutput("postable")),
+				tabPanel("pos plot (leaflet)",
+					p("click on points for time/lat/lon; big point it latest"),
+					uiOutput("qflags3"),
+					leafletOutput("leafmap", height = "600px", width = "600px"),
+					tableOutput("postable")
+				)))))
+				# tabPanel("pos plot (ggmap)",
+					# #div(style = "display: inline-block; width: 150px", 
+					# div(style = "display: inline-block; padding: 0px 20px 0px 0px; vertical-align:top; width = 300px;", uiOutput("qflags")),
+					# # div(style = "display: inline; width = 20px;", HTML("<br>")),
+					# div(style = "display: inline-block; padding: 0px 20px 0px 0px; vertical-align:top; width = 75px;", numericInput("centerlon", "center lon", value = "-75.0", step = 0.25, width = "75px")),
+					# div(style = "display: inline-block; padding: 0px 20px 0px 0px; vertical-align:top; width = 75px;", numericInput("centerlat", "center lat", value = "35.6", step = 0.25, width = "75px")),
+					# div(style = "display: inline-block; vertical-align:top; width = 150px;", sliderInput("zoom", "zoom", min = 1, max = 4, value = 4, width = "150px")),
+					# sidebarLayout(position = "right", 
+						# sidebarPanel(
+							# uiOutput("qflags"),
+							# sliderInput("zoom", "zoom:", min = 1, max = 4, value = 4, width = "150px"),
+							# div(style = "display: inline-block; padding: 0px 20px 0px 0px; vertical-align:top; width = 75px;", numericInput("centerlon", "lon:", value = "-75.0", step = 0.25, width = "75px")),
+							# div(style = "display: inline-block; padding: 0px 20px 0px 0px; vertical-align:top; width = 75px;", numericInput("centerlat", "lat:", value = "35.6", step = 0.25, width = "75px")),
+							# div(style = "display: inline;", HTML("<p>preloaded maps are centered on (-75, 35.6)</p>")),
+							# width = 3),
+							
+						# mainPanel(plotOutput("posplot", width = "100%", height = "500px"), tableOutput("postable2"))
+					#)
+				#)))))
+
+server <- function(input, output) {
+	#reactive data functions
+	sourcefiles <- reactive({
+		behaviorfname <- input$file1
+		argosfname <- input$file2
+		if(is.null(behaviorfname) & is.null(argosfname)) return(NULL)
+		
+		behavior <- NULL
+		argos <- NULL
+		bevtaglabels <- NULL
+		argtaglabels <- NULL
+		taglabels <- vector()
+		
+		if(!is.null(behaviorfname)) {
+			behavior <- read.table(behaviorfname$datapath, header = TRUE, sep = ',')
+			bevtaglabels <- paste(behavior$DeployID,behavior $Ptt, sep = " ")
+			taglabels <- c(taglabels, bevtaglabels)
+		}
+		
+		if(!is.null(argosfname)) {
+			argos <- read.table(argosfname$datapath, header = TRUE, sep = ',')
+			argtaglabels <- paste(argos$DeployID, argos$Ptt, sep = " ")
+			taglabels <- c(taglabels, argtaglabels)	
+		}
+		taglabels <- unique(taglabels)
+		list(behavior = behavior, argos = argos, taglabels = taglabels, argtaglabels = argtaglabels, bevtaglabels = bevtaglabels)
+	})
+	
+	output$tagselect <- renderUI({
+		sources <- sourcefiles()
+		if(is.null(sources)) return(NULL)
+		selectInput("tagselectinput", "select tag:", sort(sources$taglabels))
+	})
+		
+	selecttag <- reactive({
+		selection <- input$tagselectinput
+		if(is.null(selection)) return(NULL)
+		
+		sources <- sourcefiles()
+		behavior <- sources$behavior
+		argos <- sources$argos
+		
+		if(!is.null(sources$bevtaglabels)) {
+			desebev <- which(sources$bevtaglabels == selection)
+			behavior <- behavior[desebev, ]
+			if(nrow(behavior) == 0) behavior <- NULL
+		}
+		if(!is.null(sources$argtaglabels)) {
+			desearg <- which(sources$argtaglabels == selection)
+			argos <- argos[desearg, ]
+			if(nrow(argos) == 0) argos <- NULL
+		}
+		
+		list(behavior = behavior, argos = argos)
+	})
+	
+	argosdat <- reactive({
+		tag <- selecttag()
+		if(is.null(tag$argos)) return(NULL)
+		arg <- tag$argos
+		prepargos(arg)})
+		
+	divedat <- reactive({
+		tag <- selecttag()
+		if(is.null(tag$behavior)) return(NULL)
+		bev <- tag$behavior
+		prepbehavior(bev)})
+	
+	getzoom <- reactive({
+		c(input$zoom, input$centerlon, input$centerlat)
+	})
+	
+	baselinesp <- reactive({
+		sp <- input$basesp
+		out <- "other"
+		if(sp == "beakers") {
+			out <- "zca"
+		} else if(sp == "pdubs") {
+			out <- "gma"
+		}
+		out})
+		
+	baselinedep <- reactive({
+		input$mindep
+	})
+	
+	observeEvent(input$quit, {
+		stopApp(0)})
+	
+	#plot and table output function
+	output$gapstatstable  <- renderTable({
+		ddat <- divedat()
+		if(is.null(ddat)) return(NULL)
+		makegapstatstab(ddat)})
+		
+	output$divedepdur	  <- renderPlot({
+		ddat <- divedat()
+		if(is.null(ddat)) return(NULL)
+		plotdepdur(ddat, baselinesp(), baselinedep())})
+		
+	output$timebtwndive   <- renderPlot({})
+	output$divestatstable <- renderTable({
+		ddat <- divedat()
+		if(is.null(ddat)) return(NULL)
+		plotdepdurtab(ddat)})
+	
+	output$plot <- renderPlot({
+		ddat <- divedat()
+		adat <- argosdat()
+		if(is.null(ddat)) {
+			g_diveplotlive <- FALSE
+			return(NULL)
+		}
+		plotdives(ddat, adat)#, tagon = makeGMT(tagondate(), tagontime()), adat)
+		g_diveplotlive <- TRUE})
+		
+  	output$legend <- renderPlot({
+  		plotlegend(isdive = !is.null(divedat()), isargos = !is.null(argosdat()))})
+  	
+  	output$baseline_mindep <- renderUI({
+  		blsp <- baselinesp()
+  		if(blsp == "zca") {
+  			numericInput("mindep", "min. duration (minutes) for dives (baseline data):", 33, min = 0, max = 1000, step = 1)  		
+  		} else if(blsp == "gma") {
+  			numericInput("mindep", "min. depth for dives (baseline data):", 75, min = 0, max = 5000, step = 10)  
+  		} else {
+  			textOutput("nobaseline")
+  		}})
+  		
+  		output$nobaseline <- renderText({"no baseline data"})
+  	
+  	output$qflags3 <- renderUI({
+  		adat <- argosdat()
+  		if(is.null(adat)) return(NULL)
+  		argnona <- adat[which(!is.na(adat$longitude)), ]
+  		qflags <- as.character(argnona$locationquality)
+		uqflags <- unique(qflags)
+		checkboxGroupInput("qflags_checkboxes3", "qflags:", uqflags, selected = uqflags, inline = TRUE)})
+
+  	
+  	output$qflags <- renderUI({
+  		adat <- argosdat()
+  		if(is.null(adat)) return(NULL)
+  		argnona <- adat[which(!is.na(adat$longitude)), ]
+  		qflags <- as.character(argnona$locationquality)
+		uqflags <- unique(qflags)
+		checkboxGroupInput("qflags_checkboxes", "qflags:", uqflags, selected = uqflags, inline = TRUE)})
+  	
+  	output$qflags2 <- renderUI({
+  		adat <- argosdat()
+  		if(is.null(adat)) return(NULL)
+  		argnona <- adat
+  		qflags <- as.character(argnona$locationquality)
+		uqflags <- unique(qflags)
+		checkboxGroupInput("qflags_checkboxes2", "qflags:", uqflags, selected = uqflags, inline = TRUE)})
+  	
+  	# output$dateslider <- renderUI({
+  		# adat <- argosdat()
+  		# if(is.null(adat)) return(NULL)
+  		# argona <- adat[which(!is.na(adat$longitude)), ]
+  		# times <- as.POSIXct(argona$date, tz = "GMT")
+  		# range <- times[c(1, length(times))]
+  		# sliderInput("daterange", "date range:", min = min(times), max = max(times), value = range)
+  	# })
+		
+	output$leafmap <- renderLeaflet({
+		adat <- argosdat()
+		if(is.null(adat)) return(NULL)
+		leafplotargos(adat, input$qflags_checkboxes3)
+	})
+	
+	
+		
+	output$postable <- renderTable({
+		adat <- argosdat()
+		if(is.null(adat)) return(NULL)
+		tableargos(adat, input$qflags_checkboxes3)}, digits = 3)
+		
+	# output$postable2 <- renderTable({
+		# adat <- argosdat()
+		# if(is.null(adat)) return(NULL)
+		# tableargos(adat, input$qflags_checkboxes)})
+	
+	# output$posplot <- renderPlot({
+		# adat <- argosdat()
+		# if(is.null(adat)) return(NULL)
+		# plotargos(adat, input$qflags_checkboxes, getzoom())})
+		
+	output$argosuploaded <- reactive({
+		return(!is.null(argosdat()))})
+		
+	outputOptions(output, 'argosuploaded', suspendWhenHidden=FALSE)
+
+}
---
+++ guts/datafunctions.R
@@ -0,0 +1,343 @@
+#v 0.91
+
+#actual data functions for argoslook
+gDEFAULTCLON <- -75
+gDEFAULTCLAT <- 35.6
+g_lastzoom <- 4
+g_lastlon <- gDEFAULTCLON
+g_lastlat <- gDEFAULTCLAT
+
+nitro <- function(alpha) {
+	rgb(153/255, 0, 56/255, alpha)
+}
+
+carbon <- function(alpha) {
+	rgb(0, 94/255, 153/255, alpha)
+}
+
+neonemph <- function(alpha) {
+	rgb(0, 240/255, 181/255, alpha)
+}
+
+getdepths <- function(dat) {
+	dese <- which(dat$What != "Message")
+	-dat$DepthMax[dese]
+}
+
+gettimes <- function(dat) {
+	dese <- which(dat$What != "Message")
+	as.POSIXct(dat$Start[dese], tz = "GMT")
+}
+
+plotdives <- function(ddat, adat) {
+	dese <- which(ddat$what != "Message")
+	dat <- ddat[dese, ]
+	times <- as.POSIXct(dat$startposix, tz = "GMT")
+	
+	zeroday <- as.POSIXct(format(times[1], "%Y-%m-%d"), tz = "GMT")
+	lastday <- as.POSIXct(format(as.Date(times[length(times)]) + 1, "%Y-%m-%d"), tz = "GMT")
+	
+	axistimes <- seq(zeroday, lastday, by = "day")
+	axistimeslabs <- format(axistimes, "%d%h%Y")
+	
+	times_to_average <- cbind(as.POSIXct(dat$startposix, tz = "GMT"),
+		as.POSIXct(dat$endposix, tz = "GMT"))
+	averaged_times <- apply(times_to_average, 1, mean)
+	times <- as.POSIXct(averaged_times, origin = "1970-01-01", tz = "GMT")
+	par(oma = c(2, 0, 0, 0), las = 1)
+	plot(times, dat$dep, pch = 16, cex = .5, bty = 'n', xlab = "", ylab = "", axes = FALSE, ylim = c(min(dat$dep), max(dat$dep)))
+	axis(2, at = c(0, min(ddat$dep, na.rm = TRUE)), label = NA, tcl = 0)
+	axis(2, at = seq(0, min(ddat$dep, na.rm = TRUE), by = -250))
+	axis.POSIXct(1, at = axistimes, labels = axistimeslabs, las = 2, tcl = -0.9, mgp = c(0, 1.5, 0.75))
+	# axis.POSIXct(1, at = halfdayaxistimes, labels = NA, mgp = c(0, 0, 0.75))
+	if(!is.null(adat)) {
+		badies <- which(adat$locationquality %in% c("", "A", "B", "Z"))
+		argostimes <- as.POSIXct(adat$date, tz = "GMT")
+		axis.POSIXct(1, at = argostimes[badies], labels = NA, tcl = 1.25, col = neonemph(1), col.axis = nitro(1), mgp = c(0, 0, 0.75))
+		axis.POSIXct(1, at = argostimes[-badies], labels = NA, tcl = 1.25, mgp = c(0, 0, 0.75))
+		axis.POSIXct(1, at = argostimes, labels = NA, tcl = 0, mgp = c(0, 0, 0.75))
+	}
+	getgaps <- calculategaps(ddat)
+	rect(
+		as.POSIXct(getgaps$gaptimes_start, tz = "GMT"), min(ddat$dep, na.rm = TRUE),
+		as.POSIXct(getgaps$gaptimes_end, tz = "GMT"), 0,
+		col = nitro(.25), border = NA
+	)
+		
+	curtime <- Sys.time()
+	curtime_gmt <- format(curtime, tz = "GMT")
+	prev48hour <- as.POSIXct(curtime_gmt, tz = "GMT") - 60*60*48
+	abline(v = prev48hour, lty = 2, col = "purple")
+}
+
+plotlegend <- function(isdive, isargos) {
+	plot(0, 0, bty = 'n', axes = FALSE, xlab = "", ylab = "", type = 'n')
+	
+	if(isdive) {
+		leglab <- c("behavior messages", "gaps in behavior data", "48 hour cutoff")
+		legpch <- c(16, 15, NA)
+		legcol <- c("black", nitro(.5), "purple")
+		leglty <- c(NA, NA, 2)
+	
+		if(isargos) {
+			leglab <- c(leglab, "argos messages; qflag = (0, 1, 2, 3)", "argos messages; qflag = (nofix, A, B, Z)")
+			legpch <- c(legpch, 15, 15)
+			leglty <- c(leglty, NA, NA)
+			legcol <- c(legcol, "black", neonemph(1))
+		}	
+
+		legend("topleft", leglab, pch = legpch, col = legcol, lty = leglty, bty = 'n')
+	}
+}
+
+tableargos <- function(adat, qflags_checkboxes) {
+	adat <- adat[rev(1:nrow(adat)), ]
+	if(!is.null(qflags_checkboxes)) {
+		qflag <- adat$locationquality
+		dese <- which(qflag %in% qflags_checkboxes)
+		adat <- adat[dese, ]
+	}
+	adat
+}
+ 
+plotargos <- function(adat, qflags_checkboxes, zoom) {
+	argnona <- adat
+	
+	if(length(which(is.na(adat$longitude))) > 0) {
+		argnona <- adat[-which(is.na(adat$longitude)), ]
+	}
+	
+	xx <- argnona$longitude
+	yy <- argnona$latitude
+	qflag <- argnona$locationquality
+	data.df <- data.frame(xx, yy, qflag)
+	
+	if(!is.null(qflags_checkboxes)) {
+		dese <- which(qflag %in% qflags_checkboxes)
+		data.df <- data.df[dese, ]
+	}
+	
+	data.df[, 'latest'] <- "NO"
+	data.df[nrow(data.df), 'latest'] <- "YES"
+	
+	if(zoom[2] == gDEFAULTCLON & zoom[3] == gDEFAULTCLAT) {	
+		g_map <- switch(zoom[1], {mapzoom5}, {mapzoom7}, {mapzoom8}, {mapzoom9})
+		g_lastzoom <<- zoom[1]
+		g_lastlon <<- zoom[2]
+		g_lastlat <<- zoom[3]
+	} else if(zoom[1] != g_lastzoom | zoom[2] != g_lastlon | zoom[3] != g_lastlat) {
+		g_map <<- get_map(location = c(lon = zoom[2], lat = zoom[3]), zoom = c(5, 7, 8, 9)[zoom[1]], maptype = "sat")
+		g_lastzoom <<- zoom[1]
+		g_lastlon <<- zoom[2]
+		g_lastlat <<- zoom[3]
+	}
+	
+	map <- ggmap(g_map) + geom_point(aes(x = xx, y = yy, colour = qflag, shape = latest), data = data.df)
+	
+	if(nrow(data.df) > 4) {
+		lastfive <- (nrow(data.df) - 4):nrow(data.df)
+		map <- map + geom_path(arrow = arrow(), aes(x = xx, y = yy), data = data.df[lastfive, ], color = "white")
+	}
+	
+	map
+}
+
+plotdepdur <- function(ddat, baselinesp, baselinedep) {
+	dese <- which(ddat$what == "Dive")
+	dat <- ddat[dese, ]
+	sur <- ddat[which(ddat$what == "Surface"), ]
+	
+	if(baselinesp != "other" & !is.null(baselinesp)) {
+		if(baselinesp == "zca") {
+			baselinenum <- 1
+		} else if(baselinesp == "gma") {
+			baselinenum <- 2
+		}
+		
+		baseline <- base[[baselinenum]]
+		basedive <- baseline[which(baseline$what == "Dive"), ]
+		basesurf <- baseline[which(baseline$what == "Surface"), ]
+		
+		#if baselinesp is zca baselinedep is actually duration in minutes
+		#if baselinesp is gma baselinedep is depth in meters 
+		if(!is.null(baselinedep) && !is.na(baselinedep)) {
+			if(baselinenum == 2) {
+				basedive <- basedive[which(-basedive$dep > baselinedep), ]
+			} else if(baselinenum == 1) {
+				basedive <- basedive[which(basedive$dur > baselinedep*60), ]
+			}
+		}
+		
+		par(las = 1)
+		lay <- matrix(c( 1, 1, 2, 2,
+						 1, 1, 2, 2,
+						 3, 3, 4, 4, 
+						 3, 3, 4, 4,
+						 0, 5, 5, 0,
+						 0, 5, 5, 0), 6, 4, byrow = TRUE)
+		layout(lay)
+		
+		hist(dat$dur/60, nclass = 20, ylab = "", xlab = "dive duration (minutes)", main = "", col = carbon(.5))
+		a <- density(dat$dur/60)
+		b <- density(basedive$dur/60)
+		plot(c(a$x, b$x), c(a$y, b$y), type = 'n', xlab = "dive duration (minutes)", ylab = "", bty = "n")
+		lines(a, lty = 1, col = carbon(.5))
+		lines(b, lty = 1, col = nitro(.5))
+		legend("topright", c("current tag", "baseline tags"), lty = c(1, 1), col = c(carbon(.5), nitro(.5)), bty = 'n')
+		
+		hist(-dat$dep, nclass = 20, ylab = "", xlab = "depth (m)", main = "", col = carbon(.5))
+		a <- density(-dat$dep)
+		b <- density(-basedive$dep)
+		plot(c(a$x, b$x), c(a$y, b$y), type = 'n', xlab = "depth (m)", ylab = "", bty = "n")
+		lines(a, lty = 1, col = carbon(.5))
+		lines(b, lty = 1, col = nitro(.5))
+		legend("topright", c("current tag", "baseline tags"), lty = c(1, 1), col = c(carbon(.5), nitro(.5)), bty = 'n')
+			
+		hist(sur$dur/60, nclass = 20, ylab = "", xlab = "surface duration (minutes)", main = "", col = carbon(.5))
+		# a <- density(sur$dur/60)
+		# b <- density(basesurf$dur/60)
+		# plot(c(a$x, b$x), c(a$y, b$y), type = 'n', xlab = "surface duration (minutes)", ylab = "", bty = "n")
+		# lines(a, lty = 1, col = carbon(.5))
+		# lines(b, lty = 1, col = nitro(.5))
+		# legend("topright", c("current tag", "baseline tags"), lty = c(1, 1), col = c(carbon(.5), nitro(.5)), bty = 'n')
+	} else {
+		par(las = 1)
+		lay <- matrix(c( 1, 1, 2, 2,
+						 1, 1, 2, 2,
+						 0, 3, 3, 0,
+						 0, 3, 3, 0), 4, 4, byrow = TRUE)
+						 
+		layout(lay)
+		hist(dat$dur/60, nclass = 20, ylab = "", xlab = "dive duration (minutes)", main = "", col = carbon(.5))
+		hist(-dat$dep, nclass = 20, ylab = "", xlab = "depth (m)", main = "", col = carbon(.5))
+		hist(sur$dur/60, nclass = 20, ylab = "", xlab = "surface duration (minutes)", main = "", col = carbon(.5))
+	}
+}
+
+plotdepdurtab <- function(ddat) {
+	dives <- ddat[which(ddat$what == "Dive"), ]
+	surfs <- ddat[which(ddat$what == "Surface"), ]
+	
+	parameters <- c("dive duration (min)", "dive depth (m)", "surface dur (min)")
+	lowerquant <- c(quantile(dives$dur/60, .025), quantile(-dives$dep, .025), quantile(surfs$dur/60, .025))
+	median <- c(quantile(dives$dur/60, .5), quantile(-dives$dep, .5), quantile(surfs$dur/60, .5))
+	upperquant <- c(quantile(dives$dur/60, .975), quantile(-dives$dep, .975), quantile(surfs$dur/60, .975))
+	# sds <- c(sd(dives$dur/60), sd(-dives$dep), sd(surfs$dur/60))
+	ns <- c(length(dives$dur/60), length(dives$dur), length(surfs$dur))
+	sums <- c(sum(dives$dur/60), NA, sum(surfs$dur/60))
+	
+	data.frame(parameter = parameters, q0.025 = round(lowerquant, 2), q0.5 = round(median, 2), q0.975 = round(upperquant), n = ns, sum = round(sums, 2))
+}
+
+makegapstatstab <- function(ddat) {
+	parameter <- c("mean gap length (hours)", "sd gap length (hours)", "n gaps", "sum of gaps (hours)", "total ellapsed time on tag", "proportion gaps")
+	getgaps <- calculategaps(ddat)
+
+	data.frame(parameter, "current tag" = round(c(mean(getgaps$gaps, na.rm = TRUE),
+		sd(getgaps$gaps, na.rm = TRUE),
+		length(which(!is.na(getgaps$gaps))),
+		sum(getgaps$gaps, na.rm = TRUE),
+		getgaps$totalellapsedtime,
+		sum(getgaps$gaps, na.rm = TRUE) / getgaps$totalellapsedtime), 2))
+}
+
+prepargos <- function(arg) {
+	names(arg) <- tolower(names(arg))
+	dates <- paste(arg$date, "GMT", sep = " ")
+	arg$date <- dates
+	arg[order(arg$date), ]
+	arg[, c('ptt', 'date', 'longitude', 'latitude', 'locationquality', 'satellite', 'power')]
+}
+
+prepbehavior <- function(bev) {
+	names(bev) <- tolower(names(bev))
+	starts <- paste(bev$start, "GMT", sep = " ")
+	ends <- paste(bev$end, "GMT", sep = " ")
+	bev[, 'startposix'] <- as.character(starts)
+	bev[, 'endposix'] <- as.character(ends)
+	bev <- bev[order(starts), ]
+	depth <- -bev$depthmax
+	depth[which(bev$what == "Surface")] <- 0
+	bev[, 'dep'] <- depth
+	bev[, 'dur'] <- apply(bev[, c('durationmin', 'durationmax')], 1, mean)
+	behavior <- bev[, c('what', 'startposix', 'endposix', 'dep', 'dur')]
+	behavior
+}
+
+calculategaps <- function(ddat) {
+	gaps <- vector()
+	gaptimes_start <- vector()
+	gaptimes_end <- vector()
+	totalellapsedtime <- NA
+	
+	dese <- which(ddat$what == "Message")
+	message_start <- ddat[dese, 'startposix']
+	message_end <- ddat[dese, 'endposix']
+	
+	intervals_ends <- as.character(message_end)
+	intervals_starts <- as.character(message_start)
+	
+	intervals_ends <- as.POSIXct(intervals_ends, tz = "GMT")
+	intervals_starts <- as.POSIXct(intervals_starts, tz = "GMT")
+	
+	en <- length(intervals_ends)
+	
+	gaps <- as.numeric(
+		difftime(intervals_starts[2:en], 
+		intervals_ends[1:(en-1)],
+		unit = "hour"))
+		
+	gaptimes_start <- intervals_ends[1:(en-1)]
+	gaptimes_end <- intervals_starts[2:en]
+	
+	smallgaps <- which(gaps < 2/60)
+	biggaps <- which(gaps >= 2/60)
+	if(length(smallgaps) > 0) {
+		gaps[smallgaps] <- NA
+		gaptimes_start[smallgaps] <- NA
+		gaptimes_end[smallgaps] <- NA
+	}
+	totalellapsedtime <- as.numeric(difftime(intervals_ends[en], intervals_starts[1], unit = "hour"))
+	
+	# stretchid <- vector()
+	# count <- 1
+	# i <- 1
+	# while(i <= length(gaptimes_start)) {
+		# while(!(i %in% biggaps) & i <= length(gaptimes_start)) {
+			# stretchid[i] <- count
+			# i <- i + 1
+		# }
+		# if(i <= length(gaptimes_start)) {
+			# stretchid[i] <- count
+			# count <- count + 1
+			# i <- i + 1
+		# }
+	# }
+	
+	list(gaps = gaps, gaptimes_start = gaptimes_start, gaptimes_end = gaptimes_end, totalellapsedtime = totalellapsedtime)
+}
+
+leafplotargos <- function(adat, qflags) {
+		datapoints <- data.frame(lon = adat$longitude, lat = adat$latitude, qflag = as.character(adat$locationquality), date = adat$date)
+		if(length(which(is.na(datapoints$lon))) > 0) {
+			datapoints <- datapoints[-which(is.na(datapoints$lon)),]
+		}
+		dese <- which(datapoints$qflag %in% qflags)
+		datapoints <- datapoints[dese, ]
+		n <- nrow(datapoints)
+		dropback <- 4
+		if(n < 4) dropback = 0
+		radius <- rep(2, n)
+		radius[n] <- 5
+	
+		pal <- colorFactor(c(rgb(.5, .5, .5), rgb(0, .5, .75), rgb(.5, .5, 1),  rgb(0, 0, 0), rgb(1, 1, 1), rgb(1, 1, 1), rgb(1, 1, 1)), domain = c("Z", "A", "B", "0", "1", "2", "3"))
+		leaflet(datapoints) %>%
+	    addTiles(urlTemplate="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}") %>%
+	    addCircleMarkers(data = datapoints[, 1:2],
+	    	popup = paste("qflag = ", datapoints$qflag, "<br>", datapoints$date, "<br>(", round(datapoints$lat, 4), ", ", round(datapoints$lon, 4), ")", sep = ""),
+	      	radius = radius,
+	      	color = pal(datapoints$qflag)
+	     ) %>%
+		addLegend("bottomright", pal = pal, values = ~qflag, opacity = 1) %>%
+	    addPolylines(lng = ~lon[(n-dropback):n], lat = ~lat[(n-dropback):n], weight = 2, color = "white")
+}
